fca1de5122bba57af3ff6573e9255806784a415d
Leu Family <romanianfobjr@gmail.com>
2021-09-10 14:13:19 -0500

    resources endpoints created

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,21 @@
-// build your `Resource` model here
+const db = require('../../data/dbConfig')
+
+function find() {
+    return db('resources as r')
+    .select('r.*')
+    .groupBy('r.resource_id')
+}
+
+function add(resource) {
+    return db('resources').insert(resource)
+        .then(([resource_id]) => {
+            return db('resources')
+            .where('resource_id', resource_id)
+            .first()
+        })
+}
+
+module.exports = {
+    find,
+    add
+}
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,22 @@
-// build your `/api/resources` router here
+const Resources = require('./model')
+
+const router = require('express').Router()
+
+router.get('/', (req, res, next) => {
+    Resources.find()
+        .then(reso => {
+            res.json(reso)
+        })
+        .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+    const resource = req.body
+    Resources.add(resource)
+        .then(reso => {
+            res.status(201).json(reso)
+        })
+        .catch(next)
+})
+
+module.exports = router
