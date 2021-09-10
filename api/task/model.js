2083391215b11b9646d406b7f1ce331e1a0dc058
Leu Family <romanianfobjr@gmail.com>
2021-09-10 14:24:46 -0500

    tasks endpoints created

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,21 @@
-// build your `Task` model here
+const db = require('../../data/dbConfig')
+
+function find() {
+    return db('tasks as t')
+    .select('t.*')
+    .groupBy('t.task_id')
+}
+
+function add(task) {
+    return db('tasks').insert(task)
+        .then(([task_id]) => {
+            return db('tasks')
+            .where('task_id', task_id)
+            .first()
+        })
+}
+
+module.exports = {
+    find,
+    add
+}
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,22 @@
-// build your `/api/tasks` router here
+const Tasks = require('./model')
+
+const router = require('express').Router()
+
+router.get('/', (req, res, next) => {
+    Tasks.find()
+        .then(task => {
+            res.json(task)
+        })
+        .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+    const newTask = req.body
+    Tasks.add(newTask)
+        .then(task => {
+            res.status(201).json(task)
+        })
+        .catch(next)
+})
+
+module.exports = router
